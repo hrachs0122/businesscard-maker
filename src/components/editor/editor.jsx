725de5838e3457227dab6dede5cc28c384723021
hrachs0122 <gmlfk729@gmail.com>
2021-06-10 16:06:48 +0900

    add form update

--- src/components/editor/editor.jsx
+++ src/components/editor/editor.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
+import CardAddForm from '../card_add_form/card_add_form';
 import CardEditForm from '../card_edit_form/card_edit_form';
 import styles from './editor.module.css';
 
-const Editor = ({cards}) => (
+const Editor = ({cards, addCard}) => (
     <section className={styles.editor}>
         <h1 className={styles.title}>Card maker</h1>
         {
-            cards.map((card,i) => (
-                <CardEditForm card={card} key={i} />
+            cards.map(card => (
+                <CardEditForm card={card} key={card.id} />
             ))
         }
+        <CardAddForm onAdd={addCard}/> {/*callback함수 전달*/} 
     </section>
 );
 
