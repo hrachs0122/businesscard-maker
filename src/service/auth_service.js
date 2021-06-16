24ffe9a2f0a77ead22f74c410dcafd598b678320
hrachs0122 <gmlfk729@gmail.com>
2021-06-16 11:04:29 +0900

    firebase

--- src/service/auth_service.js
+++ src/service/auth_service.js
@@ -1,20 +1,30 @@
-import firebase from 'firebase';
-import firebaseApp from './firebase';
+import {firebaseAuth, githubProvider, googleProvider} from './firebase';
 
 class AuthService {
   login(providerName) {
-    const authProvider = new firebase.auth[`${providerName}AuthProvider`]();
-    return firebaseApp.auth().signInWithPopup(authProvider);
+    const authProvider = this.getProvider(providerName);
+    return firebaseAuth.signInWithPopup(authProvider);
   }
 
   logout() { 
-    firebase.auth().signOut();
+    firebaseAuth.signOut();
   }
 
   onAuthChange(onUserChanged) { // onAuthChange 라는 콜백함수를 이용해서 onUserChanged 사용자가 바뀌었을때 콜백함수 받기
-      firebase.auth().onAuthStateChanged(user => {
+      firebaseAuth.onAuthStateChanged(user => {
           onUserChanged(user); // 사용자가 바뀔때마다 전달 받도록 onUserChanged에 user를 호출
       })
+  }
+
+  getProvider(providerName) {
+    switch(providerName) {
+      case 'Google':
+        return googleProvider;
+      case 'Github':
+        return githubProvider;
+      default:
+        throw new Error(`not supported provider: ${providerName}`);  
+    }
   }
 }
 
